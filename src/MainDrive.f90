df144c2489467dda56d4b869127a57a40c8d57f0
Bo YANG <b.yang@petalmail.com>
2022-11-24 16:19:49 +0800

    Update MainDrive.f90

--- src/MainDrive.f90
+++ src/MainDrive.f90
@@ -1,6 +1,6 @@
 ! MIT License
 ! 
-! Copyright (c) 2022 Bo YANG (b.yang@petalmai.com)
+! Copyright (c) 2022 Bo YANG (b.yang@petalmail.com)
 ! 
 ! Permission is hereby granted, free of charge, to any person obtaining a copy
 ! of this software and associated documentation files (the "Software"), to deal
